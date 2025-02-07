1a75b5fe8e28ac1991c2f8c818b4b66ec6af5cff
Mohammad Ullah <ullahmohammad314@gmail.com>
2025-02-08 00:54:36 +0600

    add function to find lonely integer using eval and Function constructor

---
+++ HackerRank/lonely-integer.js
@@ -0,0 +1,15 @@
+function lonelyinteger(a) {
+    return eval(a.join('^'));
+}
+let arr = [1,2,3,4,1,2,3]
+// console.log(lonelyinteger(arr));
+
+
+
+
+///////////   an alternative solution using new Function constructor, it's more safer!   /////////
+
+let newer = arr.join('^');
+let result = new Function(`return ${newer}`)();
+
+console.log(result);
